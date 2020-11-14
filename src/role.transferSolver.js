abf49e88afdb7b45a3d0e4edc68493ce57e859db
masterkeze <861168745@qq.com>
2020-11-14 17:17:09 +0800

    feat transferSolver
    phase transition 不能很好表现出之前的并发行为。需要重新考虑下。

--- src/role.transferSolver.js
+++ src/role.transferSolver.js
@@ -1,3 +1,10 @@
+const PHASES = {
+    PREPARE : "prepare",
+    WITHDRAW : "withdraw",
+    TRANSFER : "transfer",
+    IDLE : "idle"
+}
+
 module.exports = {
     /**
      * solve transfer tasks inside the room
@@ -26,43 +33,81 @@ module.exports = {
         if (!task || !task.validate()) {
             // get a task to soleve
             task = getTransferTask(creep);
+            creep.memory.phase = PHASES.PREPARE;
         }
         if (!task) {
             creep.memory.workingOn = null;
+            creep.memory.phase = PHASES.IDLE;
             // clear stuff, and turn to idle state
         } else {
-            // to solve a task, there are three
+            const resourceType = Object.keys(task.store)[0];
+            const amount = task.store[resourceType];
+            const currentState = creep.getState();
+            let runResult,newState,config;
+            let creepStore = creep.getStore();
+            let usedCapacity = _.sum(Object.values(creepStore));
+            let totalCapacity = creep.store.getCapacity();
+            let unusedCapacity = totalCapacity - usedCapacity;
+            // same task
             if (task.name == creep.memory.workingOn){
-                // continue the current state
+                if (currentState){
+                    runResult = creep.runState();
+                    if (runResult == currentState){
+                        return;
+                    }
+                }
             } else {
-                // terminate
+                creep.memory.workingOn = task.name;
+                // register to the task
+            }
+            // transite state
+            switch (creep.memory.phase) {
+                case PHASES.TRANSFER:
+                    // transfer OK, task complete
+                    task.callback();
+                    // unregister the creep
+                    creep.memory.phase = PHASES.IDLE;
+                    creep.transitionState("",{});
+                    break;
+                case PHASES.WITHDRAW:
+                    newState = "transferOnce";
+                    config = {target : task.target, resourceType : resourceType, amount : amount};
+                    creep.memory.phase = PHASES.TRANSFER;
+                    creep.transitionState(newState,config);
+                    break;
+                case PHASES.PREPARE:
+                    newState = "withdrawOnce";
+                    config = {source : task.source,resourceType : resourceType,amount : amount};
+                    creep.memory.phase = PHASES.WITHDRAW;
+                    creep.transitionState(newState,config);
+                    break;
+                case PHASES.IDLE:
+                    if (unusedCapacity >= amount){
+                        newState = "withdrawOnce";
+                        config = {source : task.source,resourceType : resourceType,amount : amount};
+                        creep.memory.phase = PHASES.WITHDRAW;
+                        creep.transitionState(newState,config);
+                    }
+                    break;
+                default:
+                    break;
             }
         }
-
-        const currentState = creep.getState();
-        //creep.say(currentState);
-        let runResult;
-        if (currentState) {
-            runResult = creep.runState();
-            //creep.say(creep.getStore("energy"));
-            if (runResult == currentState) {
-                return;
-            }
-        }
-        let newState, config;
     }
 }
 
 
 /**
- * 给creep找任务
+ * 
  * @param {Creep} creep 
+ * @return {TransferTask} task
  */
 function getTransferTask(creep){
     /**
-     * 硬性条件
-     * 1. 一次可以运完
+     * 匹配条件
+     * 1. 就近原则
      * 2. 存活时间足够
-     * 3. 就近原则
+     * 
      */
+    return 1;
 }
