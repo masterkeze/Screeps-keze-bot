d705882849c70dac1a2c27a67bb5fe0370f02b62
Fei, Yunkai <861168745@qq.com>
2020-12-09 18:16:39 +0800

    feat creep addlock

--- screeps-ts/src/modules/lock.ts
+++ screeps-ts/src/modules/lock.ts
@@ -12,10 +12,20 @@ export namespace Lock {
             }
         }
     }
+    /**
+     * 通过对象标识，获取对象的锁信息
+     * @param  {string} id
+     */
     export function get(id: string): Lock {
         init(id);
         return Memory.lock[id];
     }
+    /**
+     * 给对象加锁，指定名称与上锁的资源包，这里不做控制，只做记录，如果重名，则会报错
+     * @param  {string} id
+     * @param  {string} name
+     * @param  {store} store
+     */
     export function add(id: string, name: string, store: store): OK | ERR_NAME_EXISTS {
         let lock = get(id);
         let lockDetail = lock.detail;
@@ -27,6 +37,11 @@ export namespace Lock {
             return OK;
         }
     }
+    /**
+     * 给对象添加匿名锁，不做控制，会返回随机生成的锁名称
+     * @param  {string} id
+     * @param  {store} store
+     */
     export function addAnonymous(id: string, store: store): string {
         let lock = get(id);
         let lockDetail = lock.detail;
@@ -41,6 +56,11 @@ export namespace Lock {
         lock.total = Helper.storeAdd(lock.total, store);
         return newName;
     }
+    /**
+     * 释放对象的锁，如果锁不存在则返回ERR_NOT_FOUND
+     * @param  {string} id
+     * @param  {string} name
+     */
     export function release(id: string, name: string):OK|ERR_NOT_FOUND{
         let lock = get(id);
         let lockDetail = lock.detail;
--- screeps-ts/src/mount/creep/extension.ts
+++ screeps-ts/src/mount/creep/extension.ts
@@ -1,7 +1,8 @@
 import states from 'state'
-import {Moment} from 'modules/moment'
-import {Helper} from 'utils'
-const directions:string = '↑↗→↘↓↙←↖';
+import { Moment } from 'modules/moment'
+import { Lock } from 'modules/lock'
+import { Helper } from 'utils'
+const directions: string = '↑↗→↘↓↙←↖';
 // creep 原型拓展
 export default class CreepExtension extends Creep {
     /**
@@ -13,24 +14,24 @@ export default class CreepExtension extends Creep {
      * 5. 如果新状态已结束，重复34
      */
     public work(): void {
-        if (!this.memory.role){
+        if (!this.memory.role) {
             // log 没找到role
             return;
         }
 
         // 注册creep
-        if (this.memory.groupID){
-            if (!this.memory.registered){
+        if (this.memory.groupID) {
+            if (!this.memory.registered) {
                 this.memory.registered = this.register(this.memory.groupID);
             }
-            if (!this.memory.registered){
+            if (!this.memory.registered) {
                 // log 没找到对应组
                 return;
             }
         }
 
         // 执行当前状态
-        let newState:string = this.runCurrentState();
+        let newState: string = this.runCurrentState();
 
     }
     /**
@@ -38,43 +39,63 @@ export default class CreepExtension extends Creep {
      * @param  {string} resourceType
      * @returns store|number
      */
-    public getMomentStore(resourceType:string):store|number{
+    public getMomentStore(resourceType?: string): store | number {
         let moment = Moment.get(this.id);
-        if (resourceType){
-            return moment.resourcesChange + this.store[resourceType];
+        if (resourceType) {
+            return moment.resourcesChange[resourceType] + this.store[resourceType];
         } else {
-            return Helper.storeAdd(moment.resourcesChange,JSON.parse(JSON.stringify(this.store)) as store);
+            return Helper.storeAdd(moment.resourcesChange, JSON.parse(JSON.stringify(this.store)) as store);
         }
     }
+    public getLockedStore(resourceType?: string): store | number {
+        let lock = Lock.get(this.id);
+        return lock.total;
+    }
+    public getUnlockedStore(resourceType?: string): store | number {
+        let lock = Lock.get(this.id);
+        return lock.total;
+    }
+    public addLock(name: string, store: store): OK | ERR_NAME_EXISTS | ERR_INVALID_ARGS {
+        let lock = Lock.get(this.id);
+        // 需要校验store的有效性
+        // 1. 不为空
+        let resourceTypes = Object.keys(store);
+        if (resourceTypes.length == 0) return ERR_INVALID_ARGS;
+        // 2. 不为负
+        let minValue = Math.min(...Object.values(store));
+        if (minValue <= 0) return ERR_INVALID_ARGS;
 
-    public register(groupID:string): number{
+        return OK;
+    }
+
+    public register(groupID: string): number {
         return 1;
     }
 
-    public getStateData(state : string) : StateMemoryData{
-        if (!this.memory.state){
-            this.memory.state = {currentState:"",data:{}};
+    public getStateData(state: string): StateMemoryData {
+        if (!this.memory.state) {
+            this.memory.state = { currentState: "", data: {} };
         }
         return this.memory.state.data[state];
     }
 
-    public runCurrentState(){
-        if (!this.memory.state){
-            this.memory.state = {currentState:"",data:{}};
+    public runCurrentState() {
+        if (!this.memory.state) {
+            this.memory.state = { currentState: "", data: {} };
         }
         let currentState = this.memory.state.currentState;
-        if (!currentState){
+        if (!currentState) {
             return "";
         }
-        
-        const stateConfig :IStateConfig = states[currentState]();
-    }
-    
-    public move(target: DirectionConstant | Creep): CreepMoveReturnCode | OK | ERR_NOT_OWNER | ERR_BUSY | ERR_NOT_IN_RANGE | ERR_INVALID_ARGS{
-        if (target instanceof Creep){
 
-        }else{
-            this.say(directions[target-1],true);
+        const stateConfig: IStateConfig = states[currentState]();
+    }
+
+    public move(target: DirectionConstant | Creep): CreepMoveReturnCode | OK | ERR_NOT_OWNER | ERR_BUSY | ERR_NOT_IN_RANGE | ERR_INVALID_ARGS {
+        if (target instanceof Creep) {
+
+        } else {
+            this.say(directions[target - 1], true);
         }
         return this._move(target);
     }
@@ -82,7 +103,7 @@ export default class CreepExtension extends Creep {
     public moveTo(
         target: RoomPosition | { pos: RoomPosition },
         opts?: MoveToOpts,
-    ): CreepMoveReturnCode | ERR_NO_PATH | ERR_INVALID_TARGET | ERR_NOT_FOUND{
-        return this._moveTo(target,opts);
+    ): CreepMoveReturnCode | ERR_NO_PATH | ERR_INVALID_TARGET | ERR_NOT_FOUND {
+        return this._moveTo(target, opts);
     }
 }
