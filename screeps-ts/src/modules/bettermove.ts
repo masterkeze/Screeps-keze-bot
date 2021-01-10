2a4a2d2847f1e8f3150c58cff9ee34b02a47c67b
masterkeze <861168745@qq.com>
2021-01-10 20:55:35 +0800

    Create bettermove.ts

---
+++ screeps-ts/src/modules/bettermove.ts
@@ -0,0 +1,7 @@
+/**
+ * This module provides methods to 
+ * 1. automatically swap creeps at the end of each tick.
+ * 2. cache path and reuse path when calling moveTo
+ * 3. automatically avoid deadly aera
+ * 4. register obstacle position ( occupied position )
+ */
